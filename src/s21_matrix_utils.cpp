308e2000b8b5a609e569e9292dbe749c60a0ab90
Runcel <nevorochka@gmail.com>
2024-06-08 18:43:55 +0700

    Size equal util and allocate fix

--- src/s21_matrix_utils.cpp
+++ src/s21_matrix_utils.cpp
@@ -4,6 +4,9 @@ void S21Matrix::AllocateMatrix(){
     matrix_ = new double*[rows_];
     for (int i = 0; i < rows_; i++){
         matrix_[i] = new double[cols_];
+        for (int j = 0; j < cols_; j++){
+            matrix_[i][j] = 0;
+        }
     }
 }
 
@@ -12,4 +15,8 @@ void S21Matrix::FreeMatrix(){
         delete[] matrix_[i];
     }
     delete[] matrix_;
+}
+
+bool S21Matrix::IsEqualSize(const S21Matrix& other){
+    return (rows_ == other.rows_ && cols_ == other.cols_);
 }
