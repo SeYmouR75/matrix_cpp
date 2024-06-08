8e0db0df103d340c7c81dba3201a95eb54c4d05a
Runcel <nevorochka@gmail.com>
2024-06-08 18:12:09 +0700

    main methods init

---
+++ src/s21_matrix_main_methods.cpp
@@ -0,0 +1,34 @@
+// #include "s21_matrix_oop.h"
+
+// bool S21Matrix::EqMatrix(const S21Matrix& other){
+//     return 0;
+// }
+
+// void S21Matrix::SumMatrix(const S21Matrix& other){
+
+// }
+// void S21Matrix::SubMatrix(const S21Matrix& other){
+
+// }
+// void S21Matrix::MulNumber(const double num){
+
+// }
+// void S21Matrix::MulMatrix(const S21Matrix& other){
+
+// }
+
+// S21Matrix S21Matrix::Transpose(){
+//     return 0;
+// }
+
+// S21Matrix S21Matrix::CalcComplements(){
+//     return 0;
+// }
+
+// double S21Matrix::Determinant(){
+//     return 0;
+// }
+
+// S21Matrix S21Matrix::InverseMatrix(){
+//     return ;
+// }
