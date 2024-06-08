9406fa99f698accd0245a466dfe7b6b801df22b4
Runcel <nevorochka@gmail.com>
2024-06-08 18:10:49 +0700

    refactoring header and cpp

--- src/s21_matrix_oop.h
+++ src/s21_matrix_oop.h
@@ -1,23 +1,35 @@
+#include <iostream>
+#include <stdexcept>
+
 class S21Matrix {
+    public:
+        S21Matrix();                         // Default constructor
+        S21Matrix(int rows, int cols);       // Parameterized constructor with number of rows and columns.
+        S21Matrix(const S21Matrix& other);   // Copy constructor
+        S21Matrix(S21Matrix&& other);        // Move constructor
+        ~S21Matrix();                        // Destructor
+
+        double& operator()(int rows, int cols);
+
+        bool EqMatrix(const S21Matrix& other);      // Comparision of two matrix
+        void SumMatrix(const S21Matrix& other);     // Sum of two matrix
+        void SubMatrix(const S21Matrix& other);     // Subtraction of two matrix
+        void MulNumber(const double num);           // Multiply the matrix by a number
+        void MulMatrix(const S21Matrix& other);     // Multiplication of two matrix
+        S21Matrix Transpose();                      // Matrix transpose
+        S21Matrix CalcComplements();                // Matrix complements
+        double Determinant();                       // Matrix determinant
+        S21Matrix InverseMatrix();                  // Inverse matrix
+
+        int GetRows(); //accessor
+        int GetCols(); //accessor
+        void SetRows(int new_rows); //mutator
+        void SetCols(int new_cols); //mutator
+
     private:
-        // Attributes
+        void AllocateMatrix();
+        void FreeMatrix();
+
         int rows_, cols_;         // Rows and columns
         double **matrix_;         // Pointer to the memory where the matrix is allocated
-
-    public:
-        S21Matrix();              // Default constructor
-        ~S21Matrix();             // Destructor
-        int getRows();
-        int getCols();
-
-        bool EqMatrix(const S21Matrix& other);
-        void SumMatrix(const S21Matrix& other);
-        void SubMatrix(const S21Matrix& other);
-        void MulNumber(const double num);
-        void MulMatrix(const S21Matrix& other);
-        S21Matrix Transpose();
-        S21Matrix CalcComplements();
-        double Determinant();
-        S21Matrix InverseMatrix(); 
-        // Other methods..
 };
