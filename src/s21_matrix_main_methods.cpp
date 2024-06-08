37e10944556f6766f06e1466f5da71df4b780b42
Runcel <nevorochka@gmail.com>
2024-06-08 23:56:51 +0700

    Eq/Sum/Sub implement

--- src/s21_matrix_main_methods.cpp
+++ src/s21_matrix_main_methods.cpp
@@ -2,22 +2,45 @@
 
 bool S21Matrix::EqMatrix(const S21Matrix& other){
     if (!IsEqualSize(other)) return false;
+    bool result = true;
 
-    for (int i = 0; i < rows_; i++){
-        for (int j = 0; j < cols_; j++){
-            if(fabs(matrix_[i][j] - other.matrix_[i][j]) < EPS) return false;
+    if (matrix_ != nullptr && other.matrix_ != nullptr){
+        for (int i = 0; i < rows_; i++){
+            for (int j = 0; j < cols_; j++){
+                if (fabs(matrix_[i][j] - other.matrix_[i][j]) > EPS){
+                    result = false;
+                    break;
+                }
+            }
+        }
+    } else result = false;
+
+    return result;
+}
+
+void S21Matrix::SumMatrix(const S21Matrix& other){
+    if (!IsEqualSize(other)) throw std::invalid_argument("different matrix dimensionality");
+    if (matrix_ == nullptr || other.matrix_ == nullptr) throw std::invalid_argument("empty pointer");
+
+
+    for (int i = 0; i < rows_; i++) {
+        for (int j = 0; j < cols_; j++) {
+            matrix_[i][j] += other.matrix_[i][j];
+        }
+    }
+}
+
+void S21Matrix::SubMatrix(const S21Matrix& other){
+    if (!IsEqualSize(other)) throw std::invalid_argument("different matrix dimensionality");
+    if (matrix_ == nullptr || other.matrix_ == nullptr) throw std::invalid_argument("empty pointer");
+
+    for (int i = 0; i < rows_; i++) {
+        for (int j = 0; j < cols_; j++) {
+            matrix_[i][j] -= other.matrix_[i][j];
         }
     }
 
-    return true;
 }
-
-// void S21Matrix::SumMatrix(const S21Matrix& other){
-
-// }
-// void S21Matrix::SubMatrix(const S21Matrix& other){
-
-// }
 // void S21Matrix::MulNumber(const double num){
 
 // }
